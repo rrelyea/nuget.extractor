b5308ad015778dad6ea63a276f9297fb09ea4f1d
Rob Relyea <rrelyea@microsoft.com>
2020-07-30 16:10:51 -0700

    set up project and have it call the nuget.packaging extraction api

---
+++ src/Program.cs
@@ -0,0 +1,37 @@
+﻿using NuGet.Common;
+using NuGet.Configuration;
+using NuGet.Packaging;
+using NuGet.Packaging.Core;
+using NuGet.Packaging.Signing;
+using NuGet.Versioning;
+using System;
+using System.Threading;
+
+namespace nuget.extractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //NOTE: this code is not best practice...just hacked it up so it would compile for right now.
+            var id = "NuGet.Protocol";
+            var version = new NuGetVersion("5.6.0");
+            PackageIdentity package = new PackageIdentity(id, version);
+            var logger = NullLogger.Instance;
+            var packageDownloader = new NuGet.Protocol.LocalPackageArchiveDownloader("c:\\packages\\source",
+                                        "c:\\packages\\other",
+                                        package,
+                                        NullLogger.Instance);
+            var clientPolicyContext = ClientPolicyContext.GetClientPolicy(Settings.LoadDefaultSettings(null), logger);
+            var pathResolver = new VersionFolderPathResolver("");
+            var extractionContext = new PackageExtractionContext(PackageSaveMode.Defaultv3, XmlDocFileSaveMode.None, clientPolicyContext, logger);
+            NuGet.Packaging.PackageExtractor.InstallFromSourceAsync(
+                                                package,
+                                                packageDownloader,
+                                                pathResolver,
+                                                extractionContext,
+                                                CancellationToken.None);
+        }
+    }
+}
